b9f06dcfdaf7d30fe74d97678af9f4f1f5a5f29a
Erik Boháč <bohac@ERIK.bohac2@spsejecna.cz>
2022-12-16 19:49:42 +0100

    Reduced javascript code

--- jsc.js
+++ jsc.js
@@ -1,39 +1,36 @@
 const timer = document.getElementById("time");
 const hex = document.getElementById("code");
 
+let hexcode;
+let hour;
+let minute;
+let second;
+
 setInterval(callFunctions, 1000);
 
 function callFunctions()
 {
-    writeTime();
-    showHex();
+    getTime();
+    writeTimeHex();
     changeBackground();
 }
 
-function writeTime()
+function getTime()
 {
     let time = new Date();
-    time.getHours() >= 10 ? timer.innerHTML = time.getHours() : timer.innerHTML = "0" + time.getHours();
-    time.getMinutes() >= 10 ? timer.innerHTML += ":" + time.getMinutes() : timer.innerHTML += ":0" + time.getMinutes();
-    time.getSeconds() >= 10 ? timer.innerHTML += ":" + time.getSeconds() : timer.innerHTML += ":0" + time.getSeconds();
+    time.getHours() >= 10 ? hour = time.getHours() : hour = "0" + time.getHours();
+    time.getMinutes() >= 10 ? minute = time.getMinutes() : minute = "0" + time.getMinutes();
+    time.getSeconds() >= 10 ? second = time.getSeconds() : second = "0" + time.getSeconds();
+    hexcode = "#" + hour + minute + second;
 }
 
-function showHex()
+function writeTimeHex()
 {
-    let time = new Date();
-    time.getHours() >= 10 ? hex.innerHTML = "#" + time.getHours() : hex.innerHTML = "#0" + time.getHours();
-    time.getMinutes() >= 10 ? hex.innerHTML += time.getMinutes() : hex.innerHTML += "0" + time.getMinutes();
-    time.getSeconds() >= 10 ? hex.innerHTML += time.getSeconds() : hex.innerHTML += "0" + time.getSeconds();
+    timer.innerHTML = hour + ":" + minute + ":" + second;
+    hex.innerHTML = hexcode;
 }
 
 function changeBackground()
 {
-    let hexcode = "#";
-
-    let time = new Date();
-    time.getHours() >= 10 ? hexcode += time.getHours() : hexcode +=  time.getHours();
-    time.getMinutes() >= 10 ? hexcode += time.getMinutes() : hexcode += "0" + time.getMinutes();
-    time.getSeconds() >= 10 ? hexcode += time.getSeconds() : hexcode += "0" + time.getSeconds();
-    
     document.body.style.backgroundColor = hexcode;
 }
